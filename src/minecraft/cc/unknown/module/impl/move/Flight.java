8e15cad102631209cef0dfd7b03754f30ef18cda
Cvrwed <qspoofing@gmail.com>
2024-06-08 11:04:19 -0500

    return polar fly

--- src/minecraft/cc/unknown/module/impl/move/Flight.java
+++ src/minecraft/cc/unknown/module/impl/move/Flight.java
@@ -1,23 +1,37 @@
 package cc.unknown.module.impl.move;
 
+import java.util.LinkedHashSet;
+
 import cc.unknown.event.impl.EventLink;
 import cc.unknown.event.impl.move.MotionEvent;
 import cc.unknown.event.impl.move.MoveEvent;
+import cc.unknown.event.impl.network.PacketEvent;
 import cc.unknown.event.impl.player.PreJumpEvent;
 import cc.unknown.event.impl.world.AirCollideEvent;
 import cc.unknown.module.impl.Module;
 import cc.unknown.module.impl.api.Category;
 import cc.unknown.module.impl.api.Info;
 import cc.unknown.module.setting.impl.ModeValue;
-import cc.unknown.utils.player.MoveUtil;
+import cc.unknown.utils.network.PacketUtil;
+import cc.unknown.utils.network.TimedPacket;
+import cc.unknown.utils.player.PlayerUtil;
 import net.minecraft.block.BlockAir;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.INetHandlerPlayClient;
+import net.minecraft.network.play.client.CPacketConfirmTransaction;
+import net.minecraft.network.play.server.SPacketEntityStatus;
+import net.minecraft.network.play.server.SPacketEntityVelocity;
 import net.minecraft.util.vec.AxisAlignedBB;
 
 @Info(name = "Flight", category = Category.Move)
 public class Flight extends Module {
 
-	private ModeValue mode = new ModeValue("Mode", "Verus", "Verus", "Vanilla");
+	private ModeValue mode = new ModeValue("Mode", "Verus", "Verus", "Vanilla", "Polar");
+	private final LinkedHashSet<TimedPacket> packetsCatch = new LinkedHashSet<>();
 	private double y;
+	private int ticks = 0;
+	private boolean damage = false;
+	private boolean release = false;
 
 	public Flight() {
 		this.registerSetting(mode);
@@ -26,7 +40,21 @@ public class Flight extends Module {
 	@Override
 	public void onEnable() {
 		y = mc.player.posY;
+		ticks = 0;
+		damage = false;
+		release = false;
 		super.onEnable();
+	}
+	
+
+	@Override
+	public void onDisable() {
+		if (PlayerUtil.inGame()) {
+			for (TimedPacket data : packetsCatch) {
+				PacketUtil.handlePacket((Packet<? extends INetHandlerPlayClient>) data.getPacket());
+			}
+		}
+		super.onDisable();
 	}
 
 	@EventLink
@@ -77,7 +105,46 @@ public class Flight extends Module {
 
 	@EventLink
 	public void onPreJump(PreJumpEvent e) {
-		e.setCancelled(true);
+		switch (mode.getMode()) {
+		case "Vanilla":
+		case "Verus":
+			e.setCancelled(true);
+			break;
+		}
 	}
 
+	@EventLink
+	public void onPacket(PacketEvent e) {
+		Packet packet = e.getPacket();
+		
+		if (e.isReceive()) {
+			switch (mode.getMode()) {
+			case "Polar":
+				
+				if (packet instanceof SPacketEntityStatus && ((SPacketEntityStatus) packet).getEntityId() == mc.player.getEntityId() && ticks <= 0) {
+					damage = true;
+					ticks = 40;
+				}
+				
+				if (packet instanceof SPacketEntityVelocity && ((SPacketEntityVelocity) packet).getEntityID() == mc.player.getEntityId() && damage) {
+					damage = false;
+					release = true;
+				}
+				
+				if (packet instanceof CPacketConfirmTransaction) {
+					if (ticks > 0) {
+						packetsCatch.add(new TimedPacket(packet, System.currentTimeMillis()));
+						e.setCancelled(true);
+						ticks--;
+					} else {
+						if (release) {
+							setToggled(false);
+						}
+					}
+				}
+				
+				break;
+			}
+		}
+	}
 }
